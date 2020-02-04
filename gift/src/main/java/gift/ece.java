5ed56bfc50f55f9edc9d8a491ddc6466df576cdb
sayantanisarkar1412 <sayantanisarkar1412@gmail.com>
2020-02-05 02:00:02 +0530

    first commit

---
+++ gift/src/main/java/gift/ece.java
@@ -0,0 +1,62 @@
+package gift;
+interface gift
+{double getw();
+
+void setw(double w);
+double getp();
+void setp(double p);
+
+}
+
+
+class sweets implements gift
+{
+double wt;
+double prc;
+public void setw(double f)
+{
+ wt=f;
+}   
+public double getw()
+{
+ return wt;
+}
+public void setp(double p)
+{
+ prc=p;
+}   
+public double getp()
+{
+ return prc;
+}
+  
+}
+
+class cho extends sweets
+{
+
+public void display()
+{
+ System.out.println("Weight: "+ this.getw());
+ System.out.println("Price: "+this.getp()); 
+}
+
+}
+
+
+public class ece {
+	public static void main(String args[])
+	{
+	   cho obj=new cho();
+	  double wi=8.5;
+	  double pp=7.5;  //scan;
+	   obj.setw(wi);
+	   obj.setp(pp);
+	   obj.display();
+
+
+
+
+	}
+
+}
